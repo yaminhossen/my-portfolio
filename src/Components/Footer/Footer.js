e8600ab6feda6fd9effd3c6f37fe88efc057e82c
yaminhossen <streatdot616@gmail.com>
2022-06-11 09:14:11 +0600

    make responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className='footer text-center sticky-bottom flex justify-center '>
+        <footer className='footer text-center flex justify-center '>
             <p className='flex items-center'>copyright <img src={copy} alt="" /><span className='text-warning'>{year}</span> all right reserve.</p>
         </footer>
     );
